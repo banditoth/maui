76005ceef1e214ea778d7062e9434431646beb2d
Eilon Lipton <Eilon@users.noreply.github.com>
2024-06-19 15:48:15 -0700

    Remove unused comments, add public API listings

--- src/Controls/src/Core/HybridWebView/HybridWebView.cs
+++ src/Controls/src/Core/HybridWebView/HybridWebView.cs
@@ -31,11 +31,6 @@ namespace Microsoft.Maui.Controls
 			set { SetValue(DefaultFileProperty, value); }
 		}
 
-		///// <summary>
-		///// Gets or sets the path for initial navigation after the content is finished loading. The default value is <c>/</c>.
-		///// </summary>
-		//public string StartPath { get; set; } = "/";
-
 		/// <summary>
 		///  The path within the app's "Raw" asset resources that contain the web app's contents. For example, if the
 		///  files are located in <c>[ProjectFolder]/Resources/Raw/hybrid_root</c>, then set this property to "hybrid_root".
@@ -57,86 +52,9 @@ namespace Microsoft.Maui.Controls
 		/// </summary>
 		public event EventHandler<HybridWebViewRawMessageReceivedEventArgs>? RawMessageReceived;
 
-		//public void Navigate(string url)
-		//{
-		//	NavigateCore(url);
-		//}
-
-		//		protected override async void OnHandlerChanged()
-		//		{
-		//			base.OnHandlerChanged();
-
-		//			await InitializeHybridWebView();
-
-		//			HybridWebViewInitialized?.Invoke(this, new HybridWebViewInitializedEventArgs()
-		//			{
-		//#if ANDROID || IOS || MACCATALYST || WINDOWS
-		//                WebView = PlatformWebView,
-		//#endif
-		//			});
-
-		//			Navigate(StartPath);
-		//		}
-
 		public void SendRawMessage(string rawMessage)
 		{
 			Handler?.Invoke(nameof(IHybridWebView.SendRawMessage), rawMessage);
-
-			//EvaluateJavaScriptAsync($"window.mauiBlazorWebView.receiveMessage({JsonSerializer.Serialize(message)})");
 		}
-
-		//		private partial Task InitializeHybridWebView();
-
-		//		private partial void NavigateCore(string url);
-
-
-		//#if !ANDROID && !IOS && !MACCATALYST && !WINDOWS
-		//		private partial Task InitializeHybridWebView() => throw null!;
-
-		//		private partial void NavigateCore(string url) => throw null!;
-		//#endif
-
-		//		public virtual void OnMessageReceived(string message)
-		//		{
-		//			var messageData = JsonSerializer.Deserialize<WebMessageData>(message);
-		//			switch (messageData?.MessageType)
-		//			{
-		//				case 0: // "raw" message (just a string)
-		//					RawMessageReceived?.Invoke(this, new HybridWebViewRawMessageReceivedEventArgs(messageData.MessageContent));
-		//					break;
-		//				default:
-		//					throw new InvalidOperationException($"Unknown message type: {messageData?.MessageType}. Message contents: {messageData?.MessageContent}");
-		//			}
-
-		//		}
-
-		//		private sealed class WebMessageData
-		//		{
-		//			public int MessageType { get; set; }
-		//			public string? MessageContent { get; set; }
-		//		}
-
-		//		internal static async Task<string?> GetAssetContentAsync(string assetPath)
-		//		{
-		//			using var stream = await GetAssetStreamAsync(assetPath);
-		//			if (stream == null)
-		//			{
-		//				return null;
-		//			}
-		//			using var reader = new StreamReader(stream);
-
-		//			var contents = reader.ReadToEnd();
-
-		//			return contents;
-		//		}
-
-		//		internal static async Task<Stream?> GetAssetStreamAsync(string assetPath)
-		//		{
-		//			if (!await FileSystem.AppPackageFileExistsAsync(assetPath))
-		//			{
-		//				return null;
-		//			}
-		//			return await FileSystem.OpenAppPackageFileAsync(assetPath);
-		//		}
 	}
 }
